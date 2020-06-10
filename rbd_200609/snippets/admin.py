8ab415daa14075ae3262e70bf4239ac754fe919c
Daniel Kim <berzzubunny@gmail.com>
2020-06-10 21:38:03 +0900

    feat: drf rubberduck Setting Filters

--- rbd_200609/snippets/admin.py
+++ rbd_200609/snippets/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from .models import Snippet
-
+from rest_framework import authtoken
 # Register your models here.
 
 
 class SnippetsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'code', 'linenos', 'language', 'style')
+    list_display = ('title', 'code', 'linenos', 'language', 'style', 'price')
 
 
-admin.site.register(Snippet, SnippetsAdmin)
+admin.site.register(Snippet, SnippetsAdmin),
+admin.register(authtoken)
