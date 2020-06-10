f3d9da793bc7151fc8b083dfda768d9f28af912a
Daniel Kim <berzzubunny@gmail.com>
2020-06-10 21:32:23 +0900

    feat: 1. Setting Viewset > ModelViewset 2. Setting SimplRouter

--- rbd_200609/snippets/urls.py
+++ rbd_200609/snippets/urls.py
@@ -1,12 +1,14 @@
-from django.urls import include, path
+from django.urls import path
+from snippets import views
 from rest_framework.routers import SimpleRouter
 from snippets.views import SnippetViewSet
 
 router = SimpleRouter()
 router.register(r'snippets', SnippetViewSet, basename='Snippet')
 
-urlpatterns = [
-    # path('', include(router.urls))
-]
+urlpatterns = router.urls
 
-urlpatterns += router.urls
+# urlpatterns = [
+#     path('snippets/', views.SnippetList.as_view()),
+#     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+# ]
--- rbd_200609/snippets/views.py
+++ rbd_200609/snippets/views.py
@@ -1,75 +1,91 @@
-from django.http import Http404
-from rest_framework import viewsets, status, filters
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from rest_framework import filters
+from rest_framework import mixins, generics, viewsets
+from snippets.serializers import SnippetModelSerializer
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
-from rest_framework.authtoken.models import Token
+from django_filters import rest_framework as filters
 
-
-
-class SnippetViewSet(viewsets.ViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    # Token = Token.objects.create(user=bunnycast)
-
-    def get_object(self, pk):
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
-
-    def list(self, request):
-        queryset = Snippet.objects.all()
-        serializer = SnippetSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        queryset = Snippet.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = SnippetSerializer(user)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-# from rest_framework import mixins
-# from rest_framework import generics
+# from rest_framework.response import Response
+# from django.shortcuts import get_object_or_404
 # from rest_framework import filters
+
+# from rest_framework.authtoken.models import Token
 #
 #
+#
+
+class SnippetFilter(filters.FilterSet):
+    min_price = filters.NumberFilter(field_name="price", lookup_expr="gte")
+    max_price = filters.NumberFilter(field_name="price", lookup_expr="lte")
+    startswith_title = filters.CharFilter(field_name="code", method="filter_start")
+
+    def filter_start(self, queryset, name, value):
+        title_filter = {f'{name}__startswith': value}
+        return queryset.filter(**title_filter)
+
+    class Meta:
+        model = Snippet
+        fields = ('code',)
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    serializer_class = SnippetModelSerializer
+    queryset = Snippet.objects.all()
+
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = SnippetFilter
+
+
+#     # Token = Token.objects.create(user=bunnycast)
+#     def get_object(self, pk):
+#         try:
+#             return Snippet.objects.get(pk=pk)
+#         except Snippet.DoesNotExist:
+#             raise Http404
+#
+#     def list(self, request):
+#         queryset = Snippet.objects.all()
+#         serializer = SnippetSerializer(queryset, many=True)
+#         return Response(serializer.data)
+#
+#     def create(self, request):
+#         serializer = SnippetSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def retrieve(self, request, pk=None):
+#         queryset = Snippet.objects.all()
+#         user = get_object_or_404(queryset, pk=pk)
+#         serializer = SnippetSerializer(user)
+#         return Response(serializer.data)
+#
+#     def update(self, request, pk=None):
+#         snippet = self.get_object(pk)
+#         serializer = SnippetSerializer(snippet, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def destroy(self, request, pk=None):
+#         snippet = self.get_object(pk)
+#         snippet.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
+
 # class SnippetList(mixins.ListModelMixin,
 #                   mixins.CreateModelMixin,
 #                   generics.GenericAPIView):
 #     queryset = Snippet.objects.all()
-#     serializer_class = SnippetSerializer
-#     filter_backends = [filters.SearchFilter]
-#     search_fields = ['title', 'code', ]
+#     serializer_class = SnippetModelSerializer
+#
+#
+#     def list(self, *args, **kwargs):
+#         print(self.request.user.username)
+#         return super().list(*args, **kwargs)
 #
 #     def get(self, request, *args, **kwargs):
+#         print(request.user.username)
 #         return self.list(request, *args, **kwargs)
 #
 #     def post(self, request, *args, **kwargs):
@@ -81,7 +97,7 @@ class SnippetViewSet(viewsets.ViewSet):
 #                     mixins.DestroyModelMixin,
 #                     generics.GenericAPIView):
 #     queryset = Snippet.objects.all()
-#     serializer_class = SnippetSerializer
+#     serializer_class = SnippetModelSerializer
 #
 #     def get(self, request, *args, **kwargs):
 #         return self.retrieve(request, *args, **kwargs)
